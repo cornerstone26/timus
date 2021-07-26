1a06aa0e5b53a365cdc498ca35faaaebd4a85b1b
hanhnguyen26 <NHHanh.2610@gmail.com>
2021-07-26 21:09:11 +0300

    Update 1009.cpp

--- 1009.cpp
+++ 1009.cpp
@@ -2,5 +2,14 @@
 using namespace std;
 
 int main() {
-
+	int N, K, res = 1;
+	cin >> N;
+	cin >> K;
+	int number[20];
+	number[1] = K - 1;
+	number[2] = number[1]*K;
+	for (int i = 3; i <= N; i++) {
+		number[i] = (number[i - 1] + number[i - 2]) * (K - 1);
+	}
+	cout << number[N];
 }
