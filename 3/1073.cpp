7795087b0f94d7989da3b0039cb4779ecc8ad303
hanhnguyen26 <NHHanh.2610@gmail.com>
2022-05-16 22:04:46 +0300

    Update 1073.cpp

--- 3/1073.cpp
+++ 3/1073.cpp
@@ -16,7 +16,6 @@ int main(){
     for (int i = 2; i <= N; i++){
         for (int j = 1 ; j*j <= i; j++){
             dp[i] = min(dp[i], dp[i - j*j] + 1);
-            //cout << i << " " << dp[i] << endl;
         }
     }
 
